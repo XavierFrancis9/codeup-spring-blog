daf68bb385a96fa1bc399f56f8f9d23d58f8974f
Xavier Francis <james.x.francis9@gmail.com>
2023-06-05 09:25:56 -0500

    adding project structure

---
+++ src/main/java/com/codeup/coduepspringblog/CoduepSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.coduepspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoduepSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoduepSpringBlogApplication.class, args);
+    }
+
+}
